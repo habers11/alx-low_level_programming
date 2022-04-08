7a69ddf893442cb38fa8dc6ad38eb684a3efcefd
habers11 <abasshamzat@yahoo.com>
2022-04-07 19:13:10 -0700

    sizeof(float)

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@ int main(void)
 {
 printf("Size of a char: $d byte(s)\n", 1);
 printf("Size of an int: $d byte(s)\n", 4);
-printf("Size of a long int: $d byte(s)\n", 8);
+printf("Size of a long int: $d byte(s)\n", 4);
 printf("Size of a long long int: $d byte(s)\n", 8);
 printf("Size of a float: $d byte(s)\n", 4);
 return (0);
